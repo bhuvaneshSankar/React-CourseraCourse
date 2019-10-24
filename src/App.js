f67111ab97e6307edff0c1ea1184e1d9edfad159
bhuvaneshSankar <bhuvaneshivuhb@gmail.com>
2019-10-24 15:14:03 +0530

    After adding menu component 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +23,21 @@ function App() {
       </header>
     </div>
   );
+}*/
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <Navbar dark color='primary'>
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
 }
 
 export default App;
